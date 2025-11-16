55ddde93d498d89ecbe41c86348b3e09e490574f
Tomas Kostrhoun <tomaskostrhoun@192.168.0.146>
2025-11-16 14:34:48 +0100

    robots.ts: Convert rules to array format for force re-crawl

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,11 +3,14 @@ import { MetadataRoute } from 'next'
 // Force recrawl - Updated 2025-11-16 - Canonical URL fix deployed
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/'],
+      },
+    ],
     sitemap: 'https://www.etfpruvodce.cz/sitemap.xml',
+    // Updated: 2025-11-16T14:00:00Z
   }
 }
